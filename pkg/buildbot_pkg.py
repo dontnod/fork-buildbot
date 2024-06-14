349847c88c6b75c9eea7e28f95e2b1d385384414
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2024-06-14 13:19:41 +0200

    setup: use setuptools-git-versioning to determine version from git tag

--- pkg/buildbot_pkg.py
+++ pkg/buildbot_pkg.py
@@ -22,9 +22,6 @@ import re
 import shutil
 import subprocess
 import sys
-from subprocess import PIPE
-from subprocess import STDOUT
-from subprocess import Popen
 
 import setuptools.command.build_py
 import setuptools.command.egg_info
@@ -140,14 +137,17 @@ def getVersion(init_file):
         return version
 
     try:
-        p = Popen(['git', 'describe', '--tags', '--always'], stdout=PIPE, stderr=STDOUT, cwd=cwd)
-        out = p.communicate()[0]
+        from setuptools_git_versioning import get_version
 
-        if (not p.returncode) and out:
-            v = gitDescribeToPep440(str(out))
-            if v:
-                return v
-    except OSError:
+        root = os.path.dirname(os.path.dirname(os.path.abspath(init_file)))
+        version = get_version(
+            config={
+                "enabled": True,
+            },
+            root=root,
+        )
+        return str(version)
+    except ImportError:
         pass
 
     try:
@@ -270,6 +270,8 @@ class EggInfoCommand(setuptools.command.egg_info.egg_info):
 def setup_www_plugin(**kw):
     package = kw['packages'][0]
     if 'version' not in kw:
+        kw.setdefault('setuptools_git_versioning', {"enabled": True})
+        kw.setdefault('setup_requires', []).append('setuptools-git-versioning>=2.0,<3')
         kw['version'] = getVersion(os.path.join(package, "__init__.py"))
 
     setup(
